7d4fe766ebf29b9b67a5798b8d056eff409d18a8
Bernardo Dias <bernardodiasdacruz@gmail.com>
2015-05-10 02:35:36 -0300

    Tab and accordeon now have button component

--- components/accordeon/Accordeon.jsx
+++ components/accordeon/Accordeon.jsx
@@ -1,17 +1,35 @@
+var AccordionButton = React.createClass({
+  toggleTab: function() {
+    console.log('accordeon: ' + this.props.component.props.handle);
+  },
+  render: function() {
+    return (
+      <button onClick={this.toggleTab}>
+          {this.props.component.props.title}
+      </button>
+    );
+  }
+});
+
 var Accordeon = React.createClass({
   toggleAccordeon: function() {
-    console.log('toggleAccordeon');
+    console.log(this.props.component.props.handle);
   },
   render: function() {
     return (
       <dl className="accordeon">
         {this.props.components.map(function(component, i) {
+          var classSelected;
+          if (i === 0) {
+            classSelected = 'selected ' + component.props.handle;
+          } else {
+            classSelected = component.props.handle;
+          }
+
           return (
-            <div className={component.props.handle} key={i}>
+            <div className={classSelected} key={i}>
               <dt>
-                <button onClick={this.toggleAccordeon}>
-                  {component.props.title}
-                </button>
+                <AccordionButton component={component} />
               </dt>
               <dd>
                 {component}
--- components/tabs/Tabs.jsx
+++ components/tabs/Tabs.jsx
@@ -1,12 +1,33 @@
+var TabButton = React.createClass({
+  toggleTab: function() {
+    console.log('tabs: ' + this.props.component.props.handle);
+  },
+  render: function() {
+    return (
+      <button onClick={this.toggleTab}>
+          {this.props.component.props.title}
+      </button>
+    );
+  }
+});
+
+
 var Tabs = React.createClass({
   render: function() {
     return (
       <div className="tabs">
         <ul className="control">
           {this.props.components.map(function(component, i) {
+            var classSelected;
+            if (i === 0) {
+              classSelected = 'selected ' + component.props.handle;
+            } else {
+              classSelected = component.props.handle;
+            }
+
             return (
-              <li className={component.props.handle} key={i}>
-                <button>{component.props.title}</button>
+              <li className={classSelected} key={i}>
+                <TabButton component={component} />
               </li>
             );
           })}
@@ -14,8 +35,15 @@ var Tabs = React.createClass({
 
         <ul className="pages">
           {this.props.components.map(function(component, i) {
+            var classSelected;
+            if (i === 0) {
+              classSelected = 'selected ' + component.props.handle;
+            } else {
+              classSelected = component.props.handle;
+            }
+
             return (
-              <li className={component.props.handle} key={i}>
+              <li className={classSelected} key={i}>
                 {component}
               </li>
             );
